c89e259f8ab64b669dfec39adf921b5587b8ac14
Raphael Marra <rapha.tavaress@gmail.com>
2020-01-06 02:24:09 -0300

    Fixing install script

--- install.sh
+++ install.sh
@@ -8,12 +8,13 @@ folders_to_create=($(find . -type d ! -path "./.git" ! -path "./.git/*" ! -path 
 
 for folder in "${folders_to_create[@]}"
 do
-	[ ! -d "$HOME/$folder" ] && echo  "$HOME/$folder"
+	[ ! -d "$HOME/$folder" ] && mkdir -p  "$HOME/$folder"
 done
 
 config_files=($(find . -type f ! -path "./.git" ! -path "./.git/*" ! -path "." ! -path "./install.sh"))
 
 for file in "${config_files[@]}"
 do
+  echo "Running: ln -sf $SCRIP_FOLDER/$file $HOME/$file"
 	ln -sf $SCRIP_FOLDER/$file $HOME/$file
 done
