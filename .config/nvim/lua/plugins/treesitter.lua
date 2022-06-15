ee0830b30d9518ac5fa9709007af35e67085d73f
Raphael Marra <rapha.tavaress@gmail.com>
2022-06-15 19:39:05 +0000

    Delete .config/nvim directory

--- .config/nvim/lua/plugins/treesitter.lua
+++
@@ -1,6 +0,0 @@
-require("nvim-treesitter.configs").setup {
-    ensure_installed = {"python", "bash", "css", "dockerfile", "go", "gomod", "hcl", "javascript"},
-    highlight = {
-        enable = true
-    },
-}
--- .config/nvim/lua/plugins/wiki.lua
+++
@@ -1,5 +0,0 @@
-vim.g.vimwiki_list = {{
-    path = '~/vimwiki/',
-    syntax = 'markdown',
-    ext = '.md'
-}}
