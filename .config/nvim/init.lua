fcafe274bfc8010bf5cccc43a4f8763a9eb21107
Raphael Marra <rapha.tavaress@gmail.com>
2023-04-05 15:48:54 -0300

    Delete .config/nvim directory

--- .config/nvim/init.lua
+++
@@ -1,4 +0,0 @@
-require('marra.settings')
-require('marra.remap')
-require('marra.packer')
-vim.cmd("colorscheme gruvbox")
--- .config/nvim/lua/marra/remap.lua
+++
@@ -1,8 +0,0 @@
--- keymap.lua
-
-vim.keymap.set({'n', 'v'}, '<leader>y', '"+y')
-vim.keymap.set({'n', 'v'}, '<leader>p', '"+gp')
-
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
