c86981e3c35396daf5ddcb3df57c26a9a22e0cb7
Raphael Marra <rapha.tavaress@gmail.com>
2024-03-25 21:56:09 -0300

    Create notification.lua

---
+++ config/awesome/notification.lua
@@ -0,0 +1,101 @@
+-- pcall(require, "luarocks.loader")
+-- local gears = require("gears")
+-- local wibox = require("wibox")
+-- local xresources = require("beautiful.xresources")
+-- local dpi = xresources.apply_dpi
+-- local naughty = require("naughty")
+-- local beautiful = require("beautiful")
+-- 
+-- naughty.persistence_enabled = true
+-- naughty.config.defaults.ontop = true
+-- naughty.config.defaults.padding = 8
+-- naughty.config.defaults.timeout = 8
+-- naughty.config.defaults.title = "System Notification"
+-- naughty.config.defaults.position = "top_right"
+-- 
+
+-- naughty.connect_signal("request::display", function(n)
+--   local app_icon = n.app_icon or "/home/user/notification.png"
+--   local heading_widget = wibox.widget {
+--     nil,
+--     {
+--       widget = wibox.widget.textbox,
+--       markup = "<b>" .. n.app_name .. "</b>",
+--     },
+--     {
+--       widget = wibox.widget.textbox,
+--       text = "X",
+--     },
+--     layout = wibox.layout.align.horizontal,
+--   }
+--   local widget = naughty.layout.box(
+--     {
+--       notification = n,
+--       type = "notification",
+--       maximum_width = dpi(380),
+--       maximum_height = dpi(980),
+--       shape = gears.shape.rectangle,
+--       widget_template = {
+--         widget = wibox.container.background,
+--         bg = beautiful.notification_bg,
+--         shape = gears.shape.rounded_rect,
+--         {
+--           layout = wibox.layout.fixed.vertical,
+--           {
+--             widget = wibox.container.background,
+--             bg = "#f7c220",
+--             fg = "#000",
+--             {
+--               widget = wibox.container.margin,
+--               margins = dpi(8),
+--               heading_widget,
+--             }
+--           },
+--           {
+--             widget = wibox.container.margin,
+--             margins = dpi(8),
+--             {
+--               widget = wibox.layout.align.horizontal,
+--               {
+--                 widget = wibox.container.margin,
+--                 top = dpi(4),
+--                 left = dpi(8),
+--                 right = dpi(8),
+--                 bottom = dpi(4),
+--                 {
+--                   widget = wibox.container.constraint,
+--                   strategy = "exact",
+--                   width = dpi(50),
+--                   height = dpi(50),
+--                   {
+--                     widget = wibox.widget.imagebox,
+--                     image = n.icon,
+--                     valign = "center",
+--                     halign = "center",
+--                   },
+--                 }
+--               },
+--               {
+--                 widget = wibox.layout.align.vertical,
+--                 {
+--                   widget = wibox.container.margin,
+--                   bottom = dpi(4),
+--                   {
+--                     widget = wibox.widget.textbox,
+--                     text = n.title,
+--                   },
+--                 },
+--                 {
+--                   widget = wibox.widget.textbox,
+--                   text = n.message,
+--                 }
+-- 
+--               }
+--             }
+--           }
+--         }
+--       }
+--     }
+--   )
+-- end)
+-- 
